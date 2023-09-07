e785f405ed654e23b2bf11fdb90363760bfb2deb
bmathus <bojko.matus@gmail.com>
2023-09-07 13:47:09 +0200

    v1

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const multer = require('multer');
+const upload = multer();
+
+require('dotenv').config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-
-
+app.post('/api/fileanalyse', upload.any(), (req, res, next) => {
+  res.json({
+    name: req.files[0].originalname,
+    type: req.files[0].mimetype,
+    size: req.files[0].size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
